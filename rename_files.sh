900878b2182e1ba6d736fcedbde5032cfce15f7f
Adam Bukolt <abukolt@zoho.com>
2024-01-22 13:44:54 +0000

    Simplify the matching of files against glob pattern, update help text.

--- rename_files.sh
+++ rename_files.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 shopt -s lastpipe
+shopt -s nullglob
 
 cur_dir="$(pwd)"
 
@@ -12,7 +13,10 @@ Usage() {
     printf "       's/[-]{1,1}[\_a-zA-Z0-9\ \-]+(\.[a-zA-Z0-9]{3,4})$/\1/g' -- remove all from dash to extension in file name, but keep extension\n"
     printf "       's/([[:upper:]])/\\\u\L\\\1/g' -- change all upper case chars to lower case\n"
     printf "       's/\\\b(.)/\\\u\\\1/g' -- change first letter of each word to upper case\n"
-    printf "       's/(\w)(\w+)+/\\\U\\\1\\\L\\\2/g' -- make first letter of each word upper case, all other lower case\n"
+    printf "       's/(\\\b\w)(\w+)(\.flac)?/\\\U\\\1\\\L\\\2\\\3/g' -- flac files, capitalise the first char of each word in the name, lower-case the\n"
+    printf "        rest, leave file extension intact\n"
+    printf "       's/(\b[A-Z]|[_][A-Z])([A-Z]+)(\.yml)?/\\\U\\\1\\\L\\\2\\\3/g' -- yaml files, with words separated by spaces and underscores, \n"
+    printf "        capitalise the first char of each word in the name and lower-case the rest, leave file extension instact\n"
     echo "    --help"
 }
 
@@ -52,37 +56,13 @@ case $yn in
     [Nn]*) echo "Aborted" ; exit  1 ;;
 esac
 
-finished=false
-
-find "$cur_dir" -type f | while read -r x; do
-    
-    if [ "$x" = "$cur_dir"/"$file_spec" ]; then
-        # echo "old_name='$x'"
-        base_name=$(basename "$x")
-        j="$(echo "$base_name" | sed -r "$replace_pattern")"
-        # echo "new_name='$j'"
-        
-        mv "$x" "$(dirname "$x")"/"$j";
-        echo "moved ""$x"" to ""$j"
-        finished=true
-    fi
-done
-# echo "finished=""$finished"
-
-if [ $finished = true ]; then
-    exit 0
-fi
-
-find "$cur_dir" -type f | while read -r x; do
+for x in $file_spec; do
     # echo "old_name='$x'"
     base_name=$(basename "$x")
     j="$(echo "$base_name" | sed -r "$replace_pattern")"
     # echo "new_name='$j'"
     
-    if [ "$x" = "$j" ]; then
-        continue
-    fi
-    
     mv "$x" "$(dirname "$x")"/"$j";
+    echo "moved ""$x"" to ""$j"
 done
 
